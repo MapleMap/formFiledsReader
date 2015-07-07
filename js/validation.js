ac58466177cd6f5e3fc488579f3b2e012d3de417
sergey.illarionov <easthunch@gmail.com>
2015-07-07 15:20:51 +0300

    fix: add spaces

--- js/validation.js
+++ js/validation.js
@@ -27,18 +27,18 @@ var Validation = (function(){
         getAllFields: function(){
             var formDataArray = settings.$form.serializeArray();
 
-            for (var i=0; i < formDataArray.length; i++){
+            for (var i=0; i < formDataArray.length; i++) {
                 if(formDataArray[i]['value'] == '') continue;
 
                 sendData[formDataArray[i]['name']] = formDataArray[i]['value'];
             }
         },
 
-        getRequiredFields: function(){
+        getRequiredFields: function() {
             var validateArray = settings.$form.find("[data-validate]"),
                 requireData = {};
 
-            for(var i=0; i < validateArray.length; i++){
+            for(var i=0; i < validateArray.length; i++) {
                 requireData[validateArray[i].getAttribute('data-validate')] = validateArray[i]['value'];
             }
 
@@ -49,7 +49,7 @@ var Validation = (function(){
     Validate = {
 
         init: function(requireData) {
-            for(var key in requireData){
+            for(var key in requireData) {
                 if (Validate[key] !== undefined) {
                     var result = Validate[key]( requireData[key] );
 
@@ -61,29 +61,29 @@ var Validation = (function(){
             }
         },
 
-        email: function(email){
+        email: function(email) {
             return (!regExp.mail.test(email)) ? false : true;
         },
 
-        phone: function(phone){
+        phone: function(phone) {
             return (phone.length < 7 ||
                     phone.length > 20 ||
                     regExp.letters.test(phone)) ? false : true;
         },
 
-        skype: function(skype){
+        skype: function(skype) {
             return (skype.length < 5) ? false : true;
         },
 
-        firstName: function(firstName){
+        firstName: function(firstName) {
             return (firstName.length < 3) ? false : true;
         },
 
-        lastName: function(lastName){
+        lastName: function(lastName) {
             return (lastName.length < 3) ? false : true;
         },
 
-        country: function(country){
+        country: function(country) {
             return (country == 'Choose country*') ? false : true;
         }
     },
@@ -98,27 +98,27 @@ var Validation = (function(){
             (errors[key] !== undefined) ? delete errors[key] : '';
         },
 
-        email: function(){
+        email: function() {
             errors.email = 'Email must be a valid';
         },
 
-        phone: function(){
+        phone: function() {
             errors.phone = 'You must specify a correct phone in international format';
         },
 
-        skype: function(){
+        skype: function() {
             errors.skype = 'A skype login is required';
         },
 
-        firstName: function(){
+        firstName: function() {
             errors.firstName = 'A firstName is required';
         },
 
-        lastName: function(){
+        lastName: function() {
             errors.lastName = 'A lastName is required';
         },
 
-        country: function(){
+        country: function() {
             errors.country = 'Country must be selected';
         }
     };
